7ec864a632a9bd6d456eca78800afd2c86f46de3
Matheuss <matheusgida@outlook.com>
2025-01-23 17:59:19 -0300

    :sparkles: feat new categories icons

--- bombahub/src/utils/categories-icons.ts
+++ bombahub/src/utils/categories-icons.ts
@@ -1,17 +1,84 @@
 import {
-    IconFlask2,
-    IconDroplet,
-    IconDropletCog,
-    IconMountain,
-    IconIcons,
-    IconProps,
-  } from "@tabler/icons-react-native";
-  
-  export const categoriesIcons: Record<string, React.ComponentType<IconProps>> = {
-    "HtQeU8GXz93V8rMkipLA": IconFlask2,      // Química
-    "bIvuOBXnFDgO0Wud67JU": IconDroplet,     // Saneamento
-    "ftJsGhPO8wz9nypuhI42": IconDropletCog,  // Petroquímica
-    "iVUjzjc0JFvUdadS0oqO": IconMountain,    // Mineração
-    "wcjmHAnI4D6n7mOAxUiq": IconIcons,        // Outros
-  };
-  
+  IconFlask2,
+  IconDroplet,
+  IconDropletCog,
+  IconMountain,
+  IconIcons,
+  IconAperture,
+  IconArchive,
+  IconArmchair,
+  IconBell,
+  IconBook,
+  IconBox,
+  IconBriefcase,
+  IconBuilding,
+  IconCamera,
+  IconCar,
+  IconChartPie,
+  IconCheck,
+  IconChevronRight,
+  IconClock,
+  IconCloud,
+  IconCode,
+  IconCoffee,
+  IconCompass,
+  IconDeviceGamepad2,
+  IconDevices,
+  IconDiamond,
+  IconDownload,
+  IconEye,
+  Icon123,
+  IconFlag,
+  IconHeart,
+  IconHome,
+  IconMusic,
+  IconPalette,
+  IconStar,
+  IconProps,
+} from "@tabler/icons-react-native";
+
+export const categoriesIcons: Record<string, React.ComponentType<IconProps>> = {
+  "HtQeU8GXz93V8rMkipLA": IconFlask2,      // Química
+  "bIvuOBXnFDgO0Wud67JU": IconDroplet,     // Saneamento
+  "ftJsGhPO8wz9nypuhI42": IconDropletCog,  // Petroquímica
+  "iVUjzjc0JFvUdadS0oqO": IconMountain,    // Mineração
+  "wcjmHAnI4D6n7mOAxUiq": IconIcons,       // Outros
+  "jye3SPTD4De1H3Y4h1S8": IconAperture, // BIM
+  "oyIGUlg9xmFXqXwpyd7t": IconIcons, // BMA
+  "igJqdjjsj7b8lZL2c0pV": IconArchive, // BMI
+  "DONEElYxe8rlYLNMaX3t": IconArmchair, //BV
+  "vUYevCno4HwM8kVi0r6M": IconBell, // APN
+  "q1mlyOoLH4z5BQt408bm": IconBook, //COMPRESSOR
+  "5EwBbhZuwsH8BBJVtNBv": IconBox, // BP
+  "c4sby0yJ0WFv0hOyG4pz": IconBriefcase, // BPE
+  "KdFwRG2WEsuPrUF5v5Qu": IconBuilding, // BV
+  "28h8LggT3wy8iVIaNjJJ": IconCamera, // BVI UC
+  "2WGPv1crLQiijC1SKwlY": IconCar, // BVW
+  "18UZ8lfjVNls035bUrh4": IconChartPie, // IBA
+  "Ch3eKZHDrQeR6JVCxMyb": IconCheck, // ILI 
+  "WIURdSGli0OpoRtDwc7r": IconChevronRight, // BVI
+  "ksq5TsA0Zp5DJMt7ry4h": IconClock, // ITAVACUO
+  "R5tweP8HzVrUvreQVMAg": IconCloud, // PQI
+  "LcLHgQxgyLrrBExHND4M": IconCode, // INI
+  "mORlTYzZd4ozkQ7DSckB": IconCoffee, // INI K/O
+  "19": IconCompass, //INI-P
+  "20": IconDeviceGamepad2, //INI-S
+  "21": IconDevices, // INI-V
+  "22": IconDiamond, //INI-VR
+  "23": IconDownload, // INK
+  "24": IconEye, // IS
+  "25": Icon123,//E/EP
+  "26": IconFlag, // RA
+  "27": IconHeart, // RT
+  "28": IconHome,
+  "29": IconMusic,
+  "30": IconPalette,
+  "31": IconStar,
+  "32": IconClock,          // Exemplo de reutilização de ícone
+  "33": IconFlask2,         // Exemplo de reutilização de ícone
+  "34": IconBriefcase,      // Exemplo de reutilização de ícone
+  "35": IconBuilding,       // Exemplo de reutilização de ícone
+  "36": IconCompass,        // Exemplo de reutilização de ícone
+  "37": IconCoffee,         // Exemplo de reutilização de ícone
+  "38": IconDevices,        // Exemplo de reutilização de ícone
+};
